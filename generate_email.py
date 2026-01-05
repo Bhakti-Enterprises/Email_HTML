941375848fb0cb543d7ecc45e7e58081cd2ee097
vivekratr <vivekratr@gmail.com>
2026-01-05 14:47:21 +0530

    Update contacts.csv to correct names and format phone numbers. Modify generate_email.py to include formatted contact numbers and email addresses in the generated emails. Enhance init_template.html to dynamically display contact information and update product images for better representation. Remove outdated plotter image file from assets for improved asset management.

--- generate_email.py
+++ generate_email.py
@@ -14,15 +14,18 @@ with open('contacts.csv', 'r', encoding='utf-8') as f:
 
 for contact in contacts:
     whatsapp_number = contact['whatsapp_number']
+    contact_number = contact['whatsapp_number']
+    contact_number = contact_number.replace('+91', '+91-')
     contact_name = contact['name']
-    
+    email_id = contact['email']
     
     whatsapp_url = f"https://wa.me/{whatsapp_number}?text=Hello%20{contact_name.replace(' ', '%20')},%20I%20am%20interested%20in%20your%20products"
     
 
     customized_html = template.replace('{{WHATSAPP_NUMBER}}', whatsapp_number)
-    customized_html = customized_html.replace('{{WHATSAPP_URL}}', whatsapp_url)
-    customized_html = customized_html.replace('{{CONTACT_NAME}}', contact_name)
+    customized_html = customized_html.replace('{{EMAIL}}', email_id)
+    # customized_html = customized_html.replace('{{CONTACT_NAME}}', contact_name)
+    customized_html = customized_html.replace('{{CONTACT_NUMBER}}', contact_number)
     
   
     filename = f"{contact_name.replace(' ', '_')}_email.html"
